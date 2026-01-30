dd02590cfd7c28220c31ea37905f3735872a8a45
Karmugilan <164208904+Karmugilan015@users.noreply.github.com>
2026-01-30 08:51:28 +0530

    Update Database.js

--- backend/DB/Database.js
+++ backend/DB/Database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async (req, res) => {
     //mongo server url
-    const url = "mongodb://127.0.0.1:27017/finmanager";
+    const url = "mongodb+srv://Bojo:<xXpQ1jl91bQlNRUH>@aution.xxfqjv0.mongodb.net/?appName=aution";
 
     const {connection} = await mongoose.connect(url);
 
