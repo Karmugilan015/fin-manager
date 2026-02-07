081e4b2b0bcd6cc8bab0804aa4f29ddfc189910e
Karmugilan <164208904+Karmugilan015@users.noreply.github.com>
2026-02-07 20:18:19 +0530

    Fix MongoDB URI variable naming and logging

--- backend/DB/Database.js
+++ backend/DB/Database.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 export const connectDB = async (req, res) => {
     //mongo server url
-    const MONGO_URI = process.env.MONGODB_URI;
+    console.log(process.env.MONGODB_URI)
+    const MONGODB_URI = process.env.MONGODB_URI;
     try {
-        await mongoose.connect(MONGO_URI);
+        console.log(MONGODB_URI)
+        await mongoose.connect(MONGODB_URI);
         console.log(`MongoDB Connection successful to ${connection.host}`);
     } catch (error) {
         console.log(`MongoDB Connection failed ${error.message}`);
